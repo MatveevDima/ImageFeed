5ad62ce653b822a2d30eca4c3b49888453c61d0b
Дмитрий <monk-dee@mail.ru>
2024-07-13 16:49:03 +0300

    Верстка кодом на экране профиля

--- ImageFeed/Helpers/UIColor+Extensions.swift
+++ ImageFeed/Helpers/UIColor+Extensions.swift
@@ -9,6 +9,8 @@ import UIKit
 
 extension UIColor {
     
+      //static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red}
+    
       //static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black}
 //    static var ypGreen: UIColor { UIColor(named: "YP Green") ?? UIColor.green }
 //    static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -9,19 +9,108 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    @IBOutlet weak var profileImage: UIImageView!
-    @IBOutlet weak var exitButton: UIButton!
-    
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var accountLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
+    var profileImage: UIImageView!
+    var exitButton: UIButton!
+    var nameLabel: UILabel!
+    var accountLabel: UILabel!
+    var descriptionLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+    }
+    
+    // MARK: - Actions
+    
+    @objc
+    func exitButtonClicked() {
     }
     
     
-    @IBAction func exitButtonClicked(_ sender: Any) {
+    // MARK: - Setup View
+    private func setupView() {
+        setupSuperView()
+        setupProfileImageView()
+        setupExitButton()
+        setupNameLabel()
+        setupAccountLabel()
+        setupDescriptionLabel()
+    }
+    
+    private func setupSuperView() {
+        view.backgroundColor = UIColor(named: "YP Black") ?? UIColor.black
+    }
+    
+    private func setupProfileImageView() {
+        profileImage = UIImageView(image: UIImage(named: "ProfileImage")!)
+        profileImage.translatesAutoresizingMaskIntoConstraints = false
+        profileImage.tintColor = .none
+        
+        view.addSubview(profileImage)
+        
+        profileImage.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        profileImage.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        profileImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
+        profileImage.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+    }
+    
+    private func setupNameLabel() {
+        
+        nameLabel = UILabel()
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.font = UIFont(name: "Arial Rounded MT Bold", size: 23)
+        nameLabel.textColor = .white
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        
+        nameLabel.leadingAnchor.constraint(equalTo: profileImage.leadingAnchor).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: 20).isActive = true
+    }
+    
+    private func setupAccountLabel() {
+        
+        accountLabel = UILabel()
+        accountLabel.text = "@ekaterina_nov"
+        accountLabel.font = UIFont(name: "Arial Rounded MT Bold", size: 13)
+        accountLabel.textColor =  UIColor(named: "YP Grey") ?? UIColor.gray
+        
+        accountLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(accountLabel)
+        
+        accountLabel.leadingAnchor.constraint(equalTo: profileImage.leadingAnchor).isActive = true
+        accountLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
+    }
+    
+    private func setupDescriptionLabel() {
+        
+        descriptionLabel = UILabel()
+        descriptionLabel.text = "Hello, world!"
+        descriptionLabel.font = UIFont(name: "Arial Rounded MT Bold", size: 13)
+        descriptionLabel.textColor = .white
+        
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabel)
+        
+        descriptionLabel.leadingAnchor.constraint(equalTo: profileImage.leadingAnchor).isActive = true
+        descriptionLabel.topAnchor.constraint(equalTo: accountLabel.bottomAnchor, constant: 8).isActive = true
+    }
+    
+    private func setupExitButton() {
+        
+        exitButton = UIButton.systemButton(
+            with: UIImage(named: "Exit")!,
+            target: self,
+            action: #selector(Self.exitButtonClicked)
+        )
+        exitButton.tintColor = UIColor(named: "YP Red") ?? UIColor.red
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(exitButton)
+        
+        exitButton.widthAnchor.constraint(equalToConstant: 44).isActive = true
+        exitButton.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8).isActive = true
+        exitButton.centerYAnchor.constraint(equalTo: profileImage.centerYAnchor).isActive = true
     }
 }
